a0ab5a93e1894a2d0c4e42b8bf1cba3359b28cdc
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2025-12-03 20:19:56 +0530

    Adding full 108_dyas_journal contents

---
+++ 108_days_Journal/Week01/Day01_Dec01-25/Concepts/basics.py
@@ -0,0 +1,24 @@
+# 0. This is a Comment
+print("Hello, World ")
+
+
+# 2. Variables -- Are containers for storing data value
+x = 10
+name = "Ram"
+
+# 3. Data Types
+## => int,float,str,bool
+
+
+
+# 4. Input/Output
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+
+# 5. Type Conversion
+age = int(input("Enter age: "))
+
+
+# 6. Operators
---
+++ 108_days_Journal/Week01/Day01_Dec01-25/Exercises/simple_arthmetic.py
@@ -0,0 +1,12 @@
+# Ask user for two numbers -> print sum, product, difference
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+sum = num1 + num2
+product = num1 * num2
+difference = num1 - num2
+
+
+print(sum)
+print(product)
+print(difference)
